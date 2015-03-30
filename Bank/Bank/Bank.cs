0f730905e854753e137a3f96bd2e1eea70d076e1
nazarzel <nazar.svyryd@gmail.com>
2015-03-30 19:52:31 +0300

    bank

---
+++ Bank/Bank/Bank.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Bank
+    {
+        int money;
+        string name;
+        int percent;
+        protected void OnPropertyChanged()
+        {
+            writeData();
+        }
+
+        public int Percent
+        {
+            get { return percent; }
+            set
+            {
+                if (value != percent)
+                {
+                    percent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int Money
+        {
+            get { return money; }
+            set
+            {
+                if (value != money)
+                {
+                    money = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void writeData()
+        {
+            string[] data = {DateTime.Now+"\n\nmoney: " + money, "\nname: " + name , "\npercent: " + percent};
+            System.IO.File.WriteAllLines(@"C:\Users\Назар\Desktop\test.txt", data);
+        }
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            bank.Money = 7000;
+            bank.Name = "Aval";
+            bank.Percent = 34;
+            bank.Percent = 13;
+        }
+
+    }
+}
